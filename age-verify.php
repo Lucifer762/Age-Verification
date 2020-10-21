d9ebe2da0a94995ff427e37a5bcc643e61a3e813
Lucifer762 <73211580+Lucifer762@users.noreply.github.com>
2020-10-21 09:43:59 +0530

    Update age-verify.php

--- age-verify.php
+++ age-verify.php
@@ -17,3 +17,4 @@
 	echo 'Your age is above 18 and your allowed here!'
 ?>
 </center>
+Your PR was submitted to a repository that is not participating in Hacktoberfest. Maintainers of the repository can add the "hacktoberfest" topic to their repository if they wish to participate. Alternatively, an individual PR can be opted-in with a maintainer adding the "hacktoberfest-accepted" label to the PR.
